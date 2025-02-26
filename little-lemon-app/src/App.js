cb001676b2388d7e0a40f9a2955411f5a7bd3165
Vasile <vas.luc.good14@gmail.com>
2025-02-26 11:04:09 +0000

    Second Commit

--- little-lemon-app/src/App.js
+++ little-lemon-app/src/App.js
@@ -3,8 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Homepage">
-    </div>
+    <html>
+      <head>
+        <meta name="description" content="Best place for italian cuisine"/>
+        <meta name="og:title" content="Little Lemon Restaurant"/>
+        <meta name="og:description" content="Best Restaurant in Chicago"/>
+        <meta name="og:image" content="Image/Logo.svg"/>
+      </head>
+      <body>
+        <Nav>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Reservation</a></li>
+            <li><a href="">Order Online</a></li>
+            <li><a href="">Login</a></li>
+          </ul>
+        </Nav>
+        <Header>
+          <img src="Images/Logo.svg"></img>
+        </Header>
+        <Main>
+
+        </Main>
+        <Footer>
+          <p>Copyright LittleLemon</p>
+        </Footer>
+      </body>
+    </html>
   );
 }
 
